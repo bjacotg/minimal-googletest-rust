3650d095c982f837f20b9e236ff3c1f96dc0f4a7
Bastien Jacot-Guillarmod <bjacotg@gmail.com>
2024-01-12 12:39:37 +0100

    Adding a test to show the limitation of HRTB

--- googletest/tests/arena_test.rs
+++ googletest/tests/arena_test.rs
@@ -34,11 +34,39 @@ impl<'a> ArenaHolder<'a, Strukt> {
     }
 }
 
+fn has_a_field<'a, Inner>(inner: Inner) -> impl Matcher<ArenaHolder<'a, Strukt>>
+where
+    Inner: for<'b> Matcher<ArenaHolder<'b, i32>>,
+{
+    struct AFieldMatcher<Inner> {
+        inner: Inner,
+    }
+
+    impl<'b, Inner> Matcher<ArenaHolder<'b, Strukt>> for AFieldMatcher<Inner>
+    where
+        Inner: for<'c> Matcher<ArenaHolder<'c, i32>>,
+    {
+        fn matches(&self, actual: &ArenaHolder<'b, Strukt>) -> MatcherResult {
+            self.inner.matches(&actual.get_a_field())
+        }
+
+        fn describe(&self, matcher_result: MatcherResult) -> googletest::description::Description {
+            todo!()
+        }
+    }
+
+    AFieldMatcher { inner }
+}
+
 #[test]
 fn check() -> Result<()> {
     let arena = vec![Strukt { a_field: 33, a_string: "something".to_string() }];
     let holder = ArenaHolder { value: &arena[0] };
 
     verify_that!(holder.get_a_field(), eq(33))?;
-    verify_that!(holder.get_a_string(), eq("something"))
+    verify_that!(holder.get_a_string(), eq("something"))?;
+    verify_that!(holder, has_a_field(eq(33)))?;
+    Ok(())
 }
+
+
